91e8fb51b94165031c2cb86ffb000c03e6460a57
Dom123x456 <dominicedgar06@gmail.com>
2022-11-15 14:34:54 +0000

    added const choicebtns

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
-
+const choiceBtns = document.querySelectorAll(".choiceBtn");
