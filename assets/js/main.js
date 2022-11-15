31185d7986ada466579d74d1d75acef3ba747822
Dom123x456 <dominicedgar06@gmail.com>
2022-11-15 14:35:50 +0000

    addded let player, let computer, let result

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,7 @@ const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
 const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
+//button click plays choice and is translated into text for the player computer and result//
